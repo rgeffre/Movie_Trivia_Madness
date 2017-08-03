a9259dcaf38d72761c905c17f95339248f233906
Renee Geffre <ReneeGeffre@RGMAC.local>
2017-08-02 22:40:49 -0400

    first updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+
+//create variables used in the game
+
+//create a timer function
+
+//create a listener for the start button that also starts the timer and displays the first questions
+
+//create and array for the questions 
+
+//compare player answers against the answers in the array 
+
+//tally how many correct answers and store that number in a variable and display it
+
+//tally how many incorrect answers and store that number in a variable and display it
+
+//tally how many questions were not answered and store that number in a variable and display it
+
